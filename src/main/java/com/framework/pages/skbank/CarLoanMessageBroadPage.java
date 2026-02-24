2fff24746d6d5b6f7370b38e8ad109217dc13f54
openthedidi <openthedidi2004@gmail.com>
2026-02-24 16:33:21 +0800

    [update]:新光車貸諮詢頁欄位驗證test

--- src/main/java/com/framework/pages/skbank/CarLoanMessageBroadPage.java
+++ src/main/java/com/framework/pages/skbank/CarLoanMessageBroadPage.java
@@ -1,9 +1,13 @@
 package com.framework.pages.skbank;
 
 
-
-
+import com.framework.enums.WaitStrategy;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CarLoanMessageBroadPage extends SkBankCommonPage {
 
@@ -12,17 +16,74 @@ public class CarLoanMessageBroadPage extends SkBankCommonPage {
     private final By NAME_INPUT = By.id("nameControl");
     private final By PHONE_INPUT = By.id("phoneControl");
     private final By CITY_SELECT = By.xpath("//span[text()='指定分行縣市']");
-    private final By CITY_OPTION = By.xpath("//span[text()='台北市']");
+
     private final By DISTRICT_SELECT = By.xpath("//span[text()='指定分行鄉鎮市區']");
-    private final By DISTRICT_OPTION = By.xpath("//span[text()='中正區']");
+
+    private final By DISTRICT_OPTIONS_LI = By.xpath("//span[text()='指定分行鄉鎮市區']/ancestor::ul/li");
+
     private final By BRANCH_SELECT = By.xpath("//span[text()='選擇分行']");
-    private final By BRANCH_OPTION = By.xpath("//span[text()='中正分行']");
+
+    private final By BRANCH_OPTION_LI = By.xpath("//span[text()='選擇分行']/ancestor::ul/li");
+
     private final By PRIVACY_CHECKBOX = By.xpath("//div[@class='consulation-request__block']//div[@class='checkbox__icon-box']");
 
+    private final By SUBMIT_BUTTON = By.xpath("//div[contains(text(),'確認送出')]");
+
+    private final By NAME_INPUT_REQUIRED_DIV = By.xpath("//div[text()='姓名']/ancestor::skbank-form-field/descendant::div[text()=' 此欄位必填 ']");
+
+    private final By NAME_INPUT_ERROR_DIV = By.xpath("//div[text()='姓名']/ancestor::skbank-form-field/descendant::div[text()=' 此欄位格式錯誤 ']");
+
+    private final By PHONE_INPUT_REQUIRED_DIV = By.xpath("//div[text()='行動電話']/ancestor::skbank-form-field/descendant::div[text()=' 此欄位必填 ']");
+
+    private final By PHONE_INPUT_ERROR_DIV = By.xpath("//div[text()='行動電話']/ancestor::skbank-form-field/descendant::div[text()=' 此欄位格式錯誤 ']");
+
+    private final By CITY_SELECT_REQUIRED_DIV = By.xpath("//div[@class='consulation-request-equal-3']//div[1]//skbank-error[1]//div[1]//div[2]");
+
+    private final By DISTRICT_SELECT_REQUIRED_DIV = By.xpath("//div[@class='form-field__field']//div[2]//skbank-error[1]//div[1]//div[2]");
+
+    private final By BRANCH_SELECT_REQUIRED_DIV = By.xpath("//div[@class='consulation-request__block']//div[3]//skbank-error[1]//div[1]//div[2]");
 
 
+    public Map<String, String> emptySubmit() {
+        click(SUBMIT_BUTTON);
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("name", getText(NAME_INPUT_REQUIRED_DIV));
+        errorMap.put("phone", getText(PHONE_INPUT_REQUIRED_DIV));
+        errorMap.put("city", getText(CITY_SELECT_REQUIRED_DIV));
+        errorMap.put("district", getText(DISTRICT_SELECT_REQUIRED_DIV));
+        errorMap.put("branch", getText(BRANCH_SELECT_REQUIRED_DIV));
+        return errorMap;
+    }
+
+    public String errorNameFormat(String Name) {
+        sendKeys(NAME_INPUT, Name);
+        return getText(NAME_INPUT_ERROR_DIV);
+    }
+
+    public String errorPhoneFormat(String Phone) {
+        sendKeys(PHONE_INPUT, Phone);
+        return getText(PHONE_INPUT_ERROR_DIV);
+
+    }
+
+    public List<String> getDistrictOptionNames(String cityName) {
+        click(CITY_SELECT);
+        click(By.xpath("//span[text()='" + cityName + "']"));
+        click(DISTRICT_SELECT);
+        return waitForElements(DISTRICT_OPTIONS_LI, WaitStrategy.PRESENT)
+                .stream().map(WebElement::getText).toList();
+    }
 
 
+    public List<String> getBranchOptionNames(String districtName, String CityName) {
+        click(CITY_SELECT);
+        click(By.xpath("//span[text()='" + CityName + "']"));
+        click(DISTRICT_SELECT);
+        click(By.xpath("//span[text()='" + districtName + "']"));
+        click(BRANCH_SELECT);
+        return waitForElements(BRANCH_OPTION_LI, WaitStrategy.PRESENT)
+                .stream().map(WebElement::getText).toList();
+    }
 
 
 }
--- src/test/java/com/framework/skbank/CarLoanMessageBroadPageTest.java
+++ src/test/java/com/framework/skbank/CarLoanMessageBroadPageTest.java
@@ -4,7 +4,11 @@ import com.framework.base.BaseTest;
 import com.framework.enums.BrowserType;
 import com.framework.pages.skbank.CarLoanMessageBroadPage;
 import com.framework.pages.skbank.SkBankSitemapPage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
 
 public class CarLoanMessageBroadPageTest extends BaseTest {
     @Override
@@ -13,7 +17,7 @@ public class CarLoanMessageBroadPageTest extends BaseTest {
     }
 
 
-    @Test
+    @Test(groups = {"smoke"})
     public void goToSiteMapPage(){
         getDriver().get("https://www.skbank.com.tw/carloan_messagebroad/");
         CarLoanMessageBroadPage carLoanMessageBroadPage = new CarLoanMessageBroadPage();
@@ -21,5 +25,64 @@ public class CarLoanMessageBroadPageTest extends BaseTest {
         assert "網站導覽".equals(skBankSitemapPage.getSiteMapTitle());
 
     }
+
+    @Test(groups = {"smoke"})
+    public void emptySubmit(){
+        getDriver().get("https://www.skbank.com.tw/carloan_messagebroad/");
+        CarLoanMessageBroadPage carLoanMessageBroadPage = new CarLoanMessageBroadPage();
+        Map<String, String> errorMessageMaps = carLoanMessageBroadPage.emptySubmit();
+        Assert.assertEquals(errorMessageMaps.get("name"), "此欄位必填");
+        Assert.assertEquals(errorMessageMaps.get("phone"), "此欄位必填");
+        Assert.assertEquals(errorMessageMaps.get("city"), "此欄位必填");
+        Assert.assertEquals(errorMessageMaps.get("district"), "此欄位必填");
+        Assert.assertEquals(errorMessageMaps.get("branch"), "此欄位必填");
+    }
+
+
+    @Test(groups = {"smoke", "regression"})
+    public void errorNameFormat(){
+        getDriver().get("https://www.skbank.com.tw/carloan_messagebroad/");
+        CarLoanMessageBroadPage carLoanMessageBroadPage = new CarLoanMessageBroadPage();
+        String errorMessage = carLoanMessageBroadPage.errorNameFormat("1");
+        Assert.assertEquals(errorMessage, "此欄位格式錯誤");
+    }
+
+    @Test(groups = {"smoke", "regression"})
+    public void errorPhoneFormat(){
+        getDriver().get("https://www.skbank.com.tw/carloan_messagebroad/");
+        CarLoanMessageBroadPage carLoanMessageBroadPage = new CarLoanMessageBroadPage();
+        String errorMessage = carLoanMessageBroadPage.errorPhoneFormat("123456789");
+        Assert.assertEquals(errorMessage, "此欄位格式錯誤");
+
+    }
+
+    @Test(groups = {"smoke", "regression"})
+    public void getDistrictOptionNames(){
+        getDriver().get("https://www.skbank.com.tw/carloan_messagebroad/");
+        CarLoanMessageBroadPage carLoanMessageBroadPage = new CarLoanMessageBroadPage();
+        String cityName = "台北市";
+        List<String> optionNames = carLoanMessageBroadPage.getDistrictOptionNames(cityName)
+                .stream().map(String::trim)
+                //把"指定分行鄉鎮市區"移除
+                .filter(name -> !name.equals("指定分行鄉鎮市區")).toList();
+        Assert.assertTrue(optionNames.contains("中正區"));
+        Assert.assertEquals(optionNames.size(), 12);
+
+    }
+
+    @Test(groups = {"smoke", "regression"})
+    public void getBranchOptionNames(){
+        getDriver().get("https://www.skbank.com.tw/carloan_messagebroad/");
+        CarLoanMessageBroadPage carLoanMessageBroadPage = new CarLoanMessageBroadPage();
+        String districtName = "中正區";
+        String cityName = "台北市";
+        List<String> optionNames = carLoanMessageBroadPage.getBranchOptionNames(districtName, cityName)
+                .stream().map(String::trim)
+                .filter(name -> !name.equals("選擇分行")).toList();
+        Assert.assertTrue(optionNames.contains("中正分行"));
+        Assert.assertEquals(optionNames.size(), 2);
+    }
+
+
 
 }
